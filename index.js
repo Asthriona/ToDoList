01a28d3164f13862fe9acd99ee93ce65832e69f4
Asthriona <im@asthriona.com>
2020-07-15 14:53:02 +0200

    console log port

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('x-powered-by', 'Poggy!');
 app.use('/', indexRouter)
-app.listen(Config.port, console.log("Server started."));
+app.listen(Config.port, console.log("App started and listening port: "+Config.port));
