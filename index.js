b31bb4ae05f5c18117d57b43469038e74f019ca2
Asthriona <im@asthriona.com>
2020-11-18 09:52:24 +0100

    add more todo

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const app = express();
 const indexRouter = require('./routes/index');
+const funRouter = require('./routes/FunStuff');
+const fuckRouter = require('./routes/FuckOff');
+const NeedRouter = require('./routes/NeedToDo');
 mongoose.connect(Config.dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -17,5 +20,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('x-powered-by', 'Poggy!');
-app.use('/', indexRouter)
+app.use('/', indexRouter);
+app.use('/Stuff_I_want_to_do_but_isnt_needed', funRouter);
+app.use('/This_can_piss_off', fuckRouter);
+app.use('/NeedToDo', NeedRouter);
 app.listen(Config.port, console.log("App started and listening port: "+Config.port));
